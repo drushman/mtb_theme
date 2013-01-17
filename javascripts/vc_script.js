630baf21e1f165c21be306b464b4181dbda1e79d
duynguyen <duynguyen@dell.(none)>
2013-01-17 09:16:05 +0700

    clear

--- javascripts/vc_script.js
+++ javascripts/vc_script.js
@@ -9,7 +9,6 @@ $(function() {
   var total_1 = 0;
   $('.view-id-videos tr img').each(function(){
     total_1 += parseInt($(this).css('width').match(/\d+/)) + 5.5;
-    console.log(total_1);
     $(this).addClass(total_1.toString());
   });
   $('.view-videos.view-display-id-block_2 tbody tr').css('width', total_1);
@@ -19,7 +18,6 @@ $(function() {
   $('.view-id-photos tr img').each(function(){
     total_2 += parseInt($(this).css('width').match(/\d+/)) + 5.5;
     $(this).addClass(total_2.toString());
-    console.log(total_2);
   });
   $('.view-photos.view-display-id-block_1 tbody tr').css('width', total_2);
   
@@ -74,17 +72,6 @@ $(function() {
         }
       )
       .jScrollPane();
-//    $('.view-photos.view-display-id-block_1 .jspContainer').append('<div class="gradient" style="display:none;"></div>');
-//    $('.view-videos.view-display-id-block_2 .jspContainer').append('<div class="gradient" style="display:none;"></div>');
-    
-//    $('.view-videos.view-display-id-block_2 .jspContainer').append('<div class="gradient" ></div>');
-    
-//    $('.view-display-id-block_2 .view-content tbody > tr > td')
-//      .appendTo('.view-id-photos.view-display-id-block_1 .view-content tbody > tr');
   }
-  
-  $(document).ready(function () {
-          
-      });
-  
+ 
 });
